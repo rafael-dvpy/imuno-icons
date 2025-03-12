92a05a8209b9107854bfa0c90c0e4e348a044e8d
ThallesCosta-Dev <costa.thalles71@gmai.com>
2025-03-11 23:34:44 -0300

    add organelles,equipment and proteins icons

--- imuno/src/data/iconData.ts
+++ imuno/src/data/iconData.ts
@@ -3,6 +3,9 @@
 import immunologyIcons from './immunologyIcons';
 import anatomyIcons from './anatomyIcons';
 import virusIcons from './virusIcons';
+import cellsAndOrganellesIcons from './cellsAndOrganellesIcons';
+import equipmentIcons from './equipamentIcons';
+import proteinIcons from './proteinsIcons';
 
 export interface IconItem {
   id: string;
@@ -14,9 +17,12 @@ export interface IconCategories {
 }
 
 const iconData: IconCategories = {
-  immunology: immunologyIcons,
-  anatomy: anatomyIcons,
-  virus: virusIcons
+  "Immunology": immunologyIcons,
+  "Anatomy": anatomyIcons,
+  "Virus": virusIcons,
+  "Cells and Organelles": cellsAndOrganellesIcons,
+  "Equipment": equipmentIcons,
+  "Proteins": proteinIcons
 };
 
 export default iconData; 
